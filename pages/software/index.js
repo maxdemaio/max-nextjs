a70589052dd1d59a1e7f977755693a14d72ffa87
Maxwell DeMaio <maxdman321@gmail.com>
2021-09-17 09:18:32 -0400

    update readme

--- pages/software/index.js
+++ pages/software/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Link from 'next/link'
 import NestedFooter from '../../components/nestedFooter'
+import softwareStyles from '../../styles/software.module.css';
 
 // export async function getStaticProps() {
 //     // TODO
@@ -17,58 +17,85 @@ import NestedFooter from '../../components/nestedFooter'
 export default function Software() {
     return (
         <>
-            {/* Software head */}
-            <Head>
-                <link rel="icon" href="/favicon.ico" />
-                <meta
-                    name="description"
-                    content="Post content here"
-                />
-                <title>Software | Max DeMaio</title>
-            </Head>
+        {/* Software head */}
+        <Head>
+            <link rel="icon" href="/favicon.ico" />
+            <meta
+                name="description"
+                content="Post content here"
+            />
+            <title>Software | Max DeMaio</title>
+        </Head>
 
-
-            {/* Tailwind CSS Typography prose */}
             <div className="mt-8 mb-8 mx-auto p-4 prose prose-lg">
-                {/*Blog post header */}
+            <main>
                 <header>
-                    <h1 class="select-none">Software</h1>
+                    <h1 className="select-none">Software</h1>
                 </header>
 
                 {/* Add this <section> tag below the existing <section> tag */}
-                <main>
-                    <div>Featured projects</div>
-                    <div>Software repos</div>
-                    <table id="myTable">
-                        <thead>
-                            <tr>
-                                <th scope="col">Title</th>
-                                <th scope="col">Description</th>
-                                <th scope="col">Language</th>
-                                <th scope="col">Created at</th>
-                                <th scope="col">Forks</th>
-                                <th scope="col">Open issues</th>
-                                <th scope="col">Size (kb)</th>
-                                <th scope="col">Stars</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td><a href="#" className="">repo name</a></td>
-                                <td>description</td>
-                                <td>language</td>
-                                <td>creations</td>
-                                <td>forks</td>
-                                <td>issues</td>
-                                <td>size kb</td>
-                                <td>stars</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </main>
+                <h2>Featured projects</h2>
+                <div className={softwareStyles.grid}>
+                    <a href="https://nextjs.org/docs" className={softwareStyles.card}>
+                        <h3>Documentation &rarr;</h3>
+                        <p>Find in-depth information about Next.js features and API.</p>
+                    </a>
 
-                <NestedFooter></NestedFooter>
-            </div>
+                    <a href="https://nextjs.org/learn" className={softwareStyles.card}>
+                        <h3>Learn &rarr;</h3>
+                        <p>Learn about Next.js in an interactive course with quizzes!</p>
+                    </a>
+
+                    <a
+                        href="https://github.com/vercel/next.js/tree/master/examples"
+                        className={softwareStyles.card}
+                    >
+                        <h3>Examples &rarr;</h3>
+                        <p>Discover and deploy boilerplate example Next.js projects.</p>
+                    </a>
+
+                    <a
+                        href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                        className={softwareStyles.card}
+                    >
+                        <h3>Deploy &rarr;</h3>
+                        <p>
+                            Instantly deploy your Next.js site to a public URL with Vercel.
+                        </p>
+                    </a>
+                </div>
+
+                <h2>Software repos</h2>
+                <table id="myTable">
+                    <thead>
+                        <tr>
+                            <th scope="col">Title</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Language</th>
+                            <th scope="col">Created at</th>
+                            <th scope="col">Forks</th>
+                            <th scope="col">Open issues</th>
+                            <th scope="col">Size (kb)</th>
+                            <th scope="col">Stars</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><a href="#" className="">repo name</a></td>
+                            <td>description</td>
+                            <td>language</td>
+                            <td>creations</td>
+                            <td>forks</td>
+                            <td>issues</td>
+                            <td>size kb</td>
+                            <td>stars</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </main>
+
+            <NestedFooter></NestedFooter>
+        </div>
         </>
     )
 }

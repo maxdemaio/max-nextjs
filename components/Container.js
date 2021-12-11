a46dc76f9f43947914e5f812ed41a3ccdeb5c453
Maxwell DeMaio <maxdman321@gmail.com>
2021-12-11 15:24:52 -0500

    add prettier

--- components/Container.js
+++ components/Container.js
@@ -1,118 +1,131 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useState, useEffect } from 'react';
-import { useTheme } from 'next-themes';
+import {useRouter} from 'next/router';
+import {useState, useEffect} from 'react';
+import {useTheme} from 'next-themes';
 import Link from 'next/link';
 
 export default function Container(props) {
-    const [mounted, setMounted] = useState(false);
-    const { resolvedTheme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const {resolvedTheme, setTheme} = useTheme();
 
-    // After mounting, we have access to the theme
-    useEffect(() => setMounted(true), []);
+  // After mounting, we have access to the theme
+  useEffect(() => setMounted(true), []);
 
-    const { children, ...customMeta } = props;
-    const router = useRouter();
+  const {children, ...customMeta} = props;
+  const router = useRouter();
 
-    // TODO, edit and add custom banner
-    const meta = {
-        title: 'Max DeMaio',
-        description: `Passionate about business, language, and computer science.`,
-        image: 'https://maxdemaio.com/static/images/me.jpg',
-        type: 'website',
-        ...customMeta
-    };
+  // TODO, edit and add custom banner
+  const meta = {
+    title: 'Max DeMaio',
+    description: `Passionate about business, language, and computer science.`,
+    image: 'https://maxdemaio.com/static/images/me.jpg',
+    type: 'website',
+    ...customMeta,
+  };
 
-    return (
-        <div>
-            <Head>
-                <title>{meta.title}</title>
-                <meta name="robots" content="follow, index" />
-                <meta content={meta.description} name="description" />
-                <meta property="og:url" content={`https://maxdemaio.com${router.asPath}`} />
-                <link rel="canonical" href={`https://maxdemaio.com${router.asPath}`} />
-                <meta property="og:type" content={meta.type} />
-                <meta property="og:site_name" content="Max DeMaio" />
-                <meta property="og:description" content={meta.description} />
-                <meta property="og:title" content={meta.title} />
-                <meta property="og:image" content={meta.image} />
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:site" content="@maxwelldemaio" />
-                <meta name="twitter:title" content={meta.title} />
-                <meta name="twitter:description" content={meta.description} />
-                <meta name="twitter:image" content={meta.image} />
-                {meta.date && (
-                    <meta property="article:published_time" content={meta.date} />
-                )}
-            </Head>
+  return (
+    <div>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="robots" content="follow, index" />
+        <meta content={meta.description} name="description" />
+        <meta
+          property="og:url"
+          content={`https://maxdemaio.com${router.asPath}`}
+        />
+        <link rel="canonical" href={`https://maxdemaio.com${router.asPath}`} />
+        <meta property="og:type" content={meta.type} />
+        <meta property="og:site_name" content="Max DeMaio" />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@maxwelldemaio" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+        {meta.date && (
+          <meta property="article:published_time" content={meta.date} />
+        )}
+      </Head>
 
-            <nav className="flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8 dark:bg-black bg-opacity-60 dark:text-gray-100">
-                <Link href="/">
-                    <a className="text-blue-400 hover:text-blue-600">Home</a>
-                </Link>
-                <Link href="/posts">
-                    <a className="text-blue-400 hover:text-blue-600">Posts</a>
-                </Link>
-                <Link href="/software">
-                    <a className="text-blue-400 hover:text-blue-600">Software</a>
-                </Link>
-                
+      <nav className="flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8 dark:bg-black bg-opacity-60 dark:text-gray-100">
+        <Link href="/">
+          <a className="text-blue-400 hover:text-blue-600">Home</a>
+        </Link>
+        <Link href="/posts">
+          <a className="text-blue-400 hover:text-blue-600">Posts</a>
+        </Link>
+        <Link href="/software">
+          <a className="text-blue-400 hover:text-blue-600">Software</a>
+        </Link>
 
-                <a
-                    href="https://twitter.com/maxwelldemaio"
-                    aria-label="Twitter Link"
-                    className="w-10 h-10 p-3 bg-gray-200 rounded dark:bg-gray-800">
-                    <svg className="w-4 h-4 text-gray-800 dark:text-gray-200" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53a4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-                    </svg>
-                </a>
+        <a
+          href="https://twitter.com/maxwelldemaio"
+          aria-label="Twitter Link"
+          className="w-10 h-10 p-3 bg-gray-200 rounded dark:bg-gray-800"
+        >
+          <svg
+            className="w-4 h-4 text-gray-800 dark:text-gray-200"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53a4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
+          </svg>
+        </a>
 
-                <a
-                    href="https://github.com/maxwelldemaio"
-                    aria-label="GitHub Link"
-                    className="w-10 h-10 p-3 bg-gray-200 rounded dark:bg-gray-800">
-                    <svg className="w-4 h-4 text-gray-800 dark:text-gray-200" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <path d="M10.07 20.503a1 1 0 0 0-1.18-.983c-1.31.24-2.963.276-3.402-.958a5.708 5.708 0 0 0-1.837-2.415a1.2 1.2 0 0 1-.167-.11a1 1 0 0 0-.93-.645h-.005a1 1 0 0 0-1 .995c-.004.815.81 1.338 1.141 1.514a4.44 4.44 0 0 1 .924 1.36c.365 1.023 1.423 2.576 4.466 2.376l.003.098l.004.268a1 1 0 0 0 2 0l-.005-.318c-.005-.19-.012-.464-.012-1.182zM20.737 5.377c.032-.125.063-.264.09-.42a6.278 6.278 0 0 0-.408-3.293a1.002 1.002 0 0 0-.615-.58c-.356-.12-1.67-.357-4.184 1.25a13.87 13.87 0 0 0-6.354 0C6.762.75 5.455.966 5.102 1.079a.997.997 0 0 0-.631.584a6.3 6.3 0 0 0-.404 3.357c.025.127.051.246.079.354a6.27 6.27 0 0 0-1.256 3.83a8.422 8.422 0 0 0 .043.921c.334 4.603 3.334 5.984 5.424 6.459a4.591 4.591 0 0 0-.118.4a1 1 0 0 0 1.942.479a1.678 1.678 0 0 1 .468-.878a1 1 0 0 0-.546-1.745c-3.454-.395-4.954-1.802-5.18-4.899a6.61 6.61 0 0 1-.033-.738a4.258 4.258 0 0 1 .92-2.713a3.022 3.022 0 0 1 .195-.231a1 1 0 0 0 .188-1.025a3.388 3.388 0 0 1-.155-.555a4.094 4.094 0 0 1 .079-1.616a7.543 7.543 0 0 1 2.415 1.18a1.009 1.009 0 0 0 .827.133a11.777 11.777 0 0 1 6.173.001a1.005 1.005 0 0 0 .83-.138a7.572 7.572 0 0 1 2.406-1.19a4.04 4.04 0 0 1 .087 1.578a3.205 3.205 0 0 1-.169.607a1 1 0 0 0 .188 1.025c.078.087.155.18.224.268A4.122 4.122 0 0 1 20 9.203a7.039 7.039 0 0 1-.038.777c-.22 3.056-1.725 4.464-5.195 4.86a1 1 0 0 0-.546 1.746a1.63 1.63 0 0 1 .466.908a3.06 3.06 0 0 1 .093.82v2.333c-.01.648-.01 1.133-.01 1.356a1 1 0 1 0 2 0c0-.217 0-.692.01-1.34v-2.35a4.881 4.881 0 0 0-.155-1.311a4.256 4.256 0 0 0-.116-.416a6.513 6.513 0 0 0 5.445-6.424A8.697 8.697 0 0 0 22 9.203a6.13 6.13 0 0 0-1.263-3.826z"></path>
-                    </svg>
-                </a>
+        <a
+          href="https://github.com/maxwelldemaio"
+          aria-label="GitHub Link"
+          className="w-10 h-10 p-3 bg-gray-200 rounded dark:bg-gray-800"
+        >
+          <svg
+            className="w-4 h-4 text-gray-800 dark:text-gray-200"
+            fill="currentColor"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <path d="M10.07 20.503a1 1 0 0 0-1.18-.983c-1.31.24-2.963.276-3.402-.958a5.708 5.708 0 0 0-1.837-2.415a1.2 1.2 0 0 1-.167-.11a1 1 0 0 0-.93-.645h-.005a1 1 0 0 0-1 .995c-.004.815.81 1.338 1.141 1.514a4.44 4.44 0 0 1 .924 1.36c.365 1.023 1.423 2.576 4.466 2.376l.003.098l.004.268a1 1 0 0 0 2 0l-.005-.318c-.005-.19-.012-.464-.012-1.182zM20.737 5.377c.032-.125.063-.264.09-.42a6.278 6.278 0 0 0-.408-3.293a1.002 1.002 0 0 0-.615-.58c-.356-.12-1.67-.357-4.184 1.25a13.87 13.87 0 0 0-6.354 0C6.762.75 5.455.966 5.102 1.079a.997.997 0 0 0-.631.584a6.3 6.3 0 0 0-.404 3.357c.025.127.051.246.079.354a6.27 6.27 0 0 0-1.256 3.83a8.422 8.422 0 0 0 .043.921c.334 4.603 3.334 5.984 5.424 6.459a4.591 4.591 0 0 0-.118.4a1 1 0 0 0 1.942.479a1.678 1.678 0 0 1 .468-.878a1 1 0 0 0-.546-1.745c-3.454-.395-4.954-1.802-5.18-4.899a6.61 6.61 0 0 1-.033-.738a4.258 4.258 0 0 1 .92-2.713a3.022 3.022 0 0 1 .195-.231a1 1 0 0 0 .188-1.025a3.388 3.388 0 0 1-.155-.555a4.094 4.094 0 0 1 .079-1.616a7.543 7.543 0 0 1 2.415 1.18a1.009 1.009 0 0 0 .827.133a11.777 11.777 0 0 1 6.173.001a1.005 1.005 0 0 0 .83-.138a7.572 7.572 0 0 1 2.406-1.19a4.04 4.04 0 0 1 .087 1.578a3.205 3.205 0 0 1-.169.607a1 1 0 0 0 .188 1.025c.078.087.155.18.224.268A4.122 4.122 0 0 1 20 9.203a7.039 7.039 0 0 1-.038.777c-.22 3.056-1.725 4.464-5.195 4.86a1 1 0 0 0-.546 1.746a1.63 1.63 0 0 1 .466.908a3.06 3.06 0 0 1 .093.82v2.333c-.01.648-.01 1.133-.01 1.356a1 1 0 1 0 2 0c0-.217 0-.692.01-1.34v-2.35a4.881 4.881 0 0 0-.155-1.311a4.256 4.256 0 0 0-.116-.416a6.513 6.513 0 0 0 5.445-6.424A8.697 8.697 0 0 0 22 9.203a6.13 6.13 0 0 0-1.263-3.826z"></path>
+          </svg>
+        </a>
 
-                
-                <button
-                    aria-label="Toggle Dark Mode"
-                    type="button"
-                    className="w-10 h-10 p-3 bg-gray-200 rounded dark:bg-gray-800"
-                    onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
-                >
-                    {mounted && (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="currentColor"
-                            stroke="currentColor"
-                            className="w-4 h-4 text-gray-800 dark:text-gray-200"
-                        >
-                            {resolvedTheme === 'dark' ? (
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
-                                />
-                            ) : (
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth={2}
-                                    d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                                />
-                            )}
-                        </svg>
-                    )}
-                </button>
-            </nav>
-            <main className="text-lg flex flex-col justify-center items-start px-8 max-w-2xl mx-auto mb-16">
-                {children}
-            </main>
-        </div>
-    );
+        <button
+          aria-label="Toggle Dark Mode"
+          type="button"
+          className="w-10 h-10 p-3 bg-gray-200 rounded dark:bg-gray-800"
+          onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
+        >
+          {mounted && (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              stroke="currentColor"
+              className="w-4 h-4 text-gray-800 dark:text-gray-200"
+            >
+              {resolvedTheme === 'dark' ? (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+                />
+              ) : (
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
+              )}
+            </svg>
+          )}
+        </button>
+      </nav>
+      <main className="text-lg flex flex-col justify-center items-start px-8 max-w-2xl mx-auto mb-16">
+        {children}
+      </main>
+    </div>
+  );
 }

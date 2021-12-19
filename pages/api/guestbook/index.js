51a187999c85b18681d0a818def2ddcd5832d501
Maxwell DeMaio <maxdman321@gmail.com>
2021-12-19 13:41:03 -0500

    add post method for prisma orm

--- pages/api/guestbook/index.js
+++ pages/api/guestbook/index.js
@@ -2,7 +2,7 @@ import { getSession } from 'next-auth/react';
 import prisma from 'lib/prisma';
 
 export default async function handler(req, res) {
-  // Retrieve all guestbook entries
+  // Retrieve all guestbook entries (no auth needed)
   if (req.method === 'GET') {
     const entries = await prisma.guestbook.findMany({
       orderBy: {
@@ -19,4 +19,34 @@ export default async function handler(req, res) {
       }))
     );
   }
+
+  // Obtain user from current session (auth needed)
+  const session = await getSession({ req });
+  const { email, name } = session.user;
+
+  if (!session) {
+    return res.status(403).send('Unauthorized');
+  }
+
+  // Post guestbook entry (auth needed)
+  if (req.method === 'POST') {
+    const newEntry = await prisma.guestbook.create({
+      data: {
+        email,
+        // Limit to 500 characters, if null just use empty string
+        body: (req.body.body || '').slice(0, 500),
+        created_by: name,
+      },
+    });
+
+    return res.status(200).json({
+      id: newEntry.id.toString(),
+      body: newEntry.body,
+      created_by: newEntry.created_by,
+      updated_at: newEntry.updated_at,
+    });
+  }
+
+  // Any other methods
+  return res.send('Method not allowed.');
 }

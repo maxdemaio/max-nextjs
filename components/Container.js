3f8d6b1f04e027b747319f6433b349cb7823a07f
Maxwell DeMaio <maxdman321@gmail.com>
2021-12-19 20:10:15 -0500

    update gitignore for cypress screenshots and fix e2e

--- components/Container.js
+++ components/Container.js
@@ -56,6 +56,7 @@ export default function Container(props) {
       <nav className="flex items-center justify-between w-full max-w-4xl p-8 mx-auto my-0 text-gray-900 bg-white sticky-nav md:my-8 dark:bg-black bg-opacity-60 dark:text-gray-100">
         <Link href="/">
           <a
+            id="home-nav"
             aria-label="Home link"
             className="text-blue-400 hover:text-blue-600"
           >

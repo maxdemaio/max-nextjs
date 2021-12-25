08679d0431f31a6617fc979c6ba02afe29b8fa80
Maxwell DeMaio <maxdman321@gmail.com>
2021-12-25 12:51:07 -0500

    make id slug data fetch more readable

---
+++ components/BlogFooter.js
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import CcName from '@/components/CcName';
+
+export default function BlogFooter() {
+  const moveToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
+  return (
+    <footer className="mt-8">
+      <Link href="/posts">
+        <a className="underline text-blue-400 hover:text-blue-600">← Back</a>
+      </Link>
+      {/* To top button */}
+      <button className="cursor-pointer float-right" onClick={moveToTop}>
+        <a className="underline text-blue-400 hover:text-blue-600">↑ Top</a>
+      </button>
+      <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mt-8 mb-8" />
+      <p className="my-para">
+        <strong>max overflow</strong> is a blog about business, language, and
+        computer science. Sometimes my thoughts overflow and end up here for
+        safekeeping.
+      </p>
+      <p className="my-para">
+        For corrections (typos, factual inaccuracies), please file a{' '}
+        <a
+          href="https://github.com/maxdemaio/max-nextjs/issues"
+          className="underline text-blue-400 hover:text-blue-600"
+        >
+          GitHub issue
+        </a>
+        .
+      </p>
+      <CcName></CcName>
+    </footer>
+  );
+}
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
@@ -7,51 +6,25 @@ import { tomorrow } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 import { getAllPostIds, getPostData } from '@/lib/posts';
 import DateComp from '@/components/DateComp';
 import Container from '@/components/Container';
-import CcName from '@/components/CcName';
+import BlogFooter from '@/components/BlogFooter';
 
-export async function getStaticProps({ params }) {
-  // We added the async keyword to getPostData in lib/posts.js
-  // We need to use await for remark
-  // Async/await allows us to fetch data asynchronously.
-  const postData = await getPostData(params.id);
-  return {
-    props: {
-      postData,
-    },
-  };
-}
-
-export async function getStaticPaths() {
-  const paths = getAllPostIds();
-  return {
-    paths,
-    fallback: false,
-  };
-}
-
-export default function Post({ postData }) {
-  const blogTitle = 'max overflow';
-
-  const moveToTop = () => {
-    window.scrollTo(0, 0);
-  };
-
+export default function Post({ id, html, meta }) {
   return (
-    <Container title={postData.title}>
+    <Container title={meta.title}>
       <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
         <div className="mb-8">
-          <h1 className="my-h1">{postData.title}</h1>
+          <h1 className="my-h1">{meta.title}</h1>
           <div className="text-gray-500">
-            <DateComp dateString={postData.date} />
+            <DateComp dateString={meta.date} />
           </div>
-          <div className="sr-only">{postData.id}</div>
+          <div className="sr-only">{id}</div>
         </div>
 
         <div className="prose prose-lg dark:prose-dark max-w-none w-full">
           {/* Blog post content */}
           <ReactMarkdown
             remarkPlugins={[remarkGfm]}
-            children={postData.html}
+            children={html}
             components={{
               code({ node, inline, className, children, ...props }) {
                 const match = /language-(\w+)/.exec(className || '');
@@ -73,36 +46,30 @@ export default function Post({ postData }) {
           ></ReactMarkdown>
         </div>
 
-        {/* Blog post footer */}
-        <footer className="mt-8">
-          <Link href="/posts">
-            <a className="underline text-blue-400 hover:text-blue-600">
-              ← Back
-            </a>
-          </Link>
-          {/* To top button */}
-          <button className="cursor-pointer float-right" onClick={moveToTop}>
-            <a className="underline text-blue-400 hover:text-blue-600">↑ Top</a>
-          </button>
-          <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mt-8 mb-8" />
-          <p className="my-para">
-            <strong>{blogTitle}</strong> is a blog about business, language, and
-            computer science. Sometimes my thoughts overflow and end up here for
-            safekeeping.
-          </p>
-          <p className="my-para">
-            For corrections (typos, factual inaccuracies), please file a{' '}
-            <a
-              href="https://github.com/maxdemaio/max-nextjs/issues"
-              className="underline text-blue-400 hover:text-blue-600"
-            >
-              GitHub issue
-            </a>
-            .
-          </p>
-          <CcName></CcName>
-        </footer>
+        <BlogFooter />
       </article>
     </Container>
   );
+}
+
+export async function getStaticProps({ params }) {
+  // We added the async keyword to getPostData in lib/posts.js
+  // We need to use await for remark
+  // Async/await allows us to fetch data asynchronously.
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      id: postData.id,
+      html: postData.html,
+      meta: postData.meta,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
 }

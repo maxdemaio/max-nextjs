c5245cae51cf3cca97d8fdbabbbe386a39c67b9f
Maxwell DeMaio <maxdman321@gmail.com>
2021-12-31 18:24:57 -0500

    add fade in props to container of website

--- components/Container.js
+++ components/Container.js
@@ -179,7 +179,7 @@ export default function Container(props) {
           )}
         </button>
       </nav>
-      <main className="text-lg flex flex-col justify-center items-start px-8 max-w-3xl mx-auto mb-16">
+      <main className="my-fade text-lg flex flex-col justify-center items-start px-8 max-w-3xl mx-auto mb-16">
         {children}
       </main>
     </div>

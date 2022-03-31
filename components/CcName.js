14096a9f826737ded8e46a5f6eb7a31ac4a4c834
Maxwell DeMaio <maxdman321@gmail.com>
2022-03-30 22:19:15 -0400

    rm cc license, add new footer

--- components/CcName.js
+++ components/CcName.js
@@ -1,18 +1,7 @@
 export default function CcName() {
   return (
-    <div>
-      <span>
-        <a
-          className="underline text-blue-400 hover:text-blue-600"
-          href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
-        >
-          CC BY-NC-SA 4.0
-        </a>
-        <span className="text-gray-700 dark:text-gray-400">
-          {' '}
-          {new Date().getFullYear()} © Maxwell DeMaio
-        </span>
-      </span>
+    <div className="text-gray-700 dark:text-gray-400">
+      Made with ❤️ by Max, © {new Date().getFullYear()}
     </div>
   );
 }

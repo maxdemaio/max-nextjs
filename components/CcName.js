7f8893029cc33520de873af84d0bea25f72f3445
Maxwell DeMaio <maxdman321@gmail.com>
2022-03-30 22:23:17 -0400

    rm comma

--- components/CcName.js
+++ components/CcName.js
@@ -1,7 +1,7 @@
 export default function CcName() {
   return (
     <div className="text-gray-700 dark:text-gray-400">
-      Made with ❤️ by Max, © {new Date().getFullYear()}
+      Made with ❤️ by Max © {new Date().getFullYear()}
     </div>
   );
 }

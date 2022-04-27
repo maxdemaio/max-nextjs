524def35231d112f45cdbf0e53114664c8dc1b8c
Maxwell DeMaio <maxdman321@gmail.com>
2022-04-27 00:23:43 -0400

    rm music details

--- pages/music.js
+++ pages/music.js
@@ -29,70 +29,13 @@ export default function Music() {
           </svg>
         </h1>
         <p className="my-para">
-          A place for all things music related in my life. Here you'll find my
-          favorite tracks, original music, and covers.
+          A place for all things music related in my life.
         </p>
-      </section>
-
-      <section>
-        <h2 className="my-h2">My Featured Tracks</h2>
-        <p className="my-para">
-          My favorite tracks that I've created/covered. For all my music, feel
-          free to check out{' '}
-          <a
-            href="https://soundcloud.com/maxdemaio"
-            className="font-medium underline decoration-orange-300 decoration-2 text-gray-900 dark:text-gray-100"
-          >
-            my SoundCloud
-          </a>
-          .
-        </p>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
-          <div className="flex flex-col w-full">
-            <h3 className="my-h3 pl-3">Originals</h3>
-            <MyTrack
-              artist=""
-              songUrl="https://soundcloud.com/maxdemaio/whiplash"
-              title="whiplash"
-              original={true}
-            />
-            <MyTrack
-              artist=""
-              songUrl="https://soundcloud.com/maxdemaio/from-the-heart"
-              title="from the heart"
-              original={true}
-            />
-          </div>
-          <div className="flex flex-col w-full">
-            <h3 className="my-h3 pl-3">Covers</h3>
-            <MyTrack
-              artist="jorja smith & burna boy"
-              songUrl="https://soundcloud.com/maxdemaio/be-honest-jorja-smith-burna-boy-cover"
-              title="be honest"
-              original={false}
-            />
-            <MyTrack
-              artist="keshi"
-              songUrl="https://soundcloud.com/maxdemaio/less-of-you-keshi-cover"
-              title="less of you"
-              original={false}
-            />
-          </div>
-        </div>
       </section>
 
       <h2 className="my-h2">Top Spotify Tracks</h2>
       <p className="my-para">
-        My most played tracks on Spotify over the last 4 weeks. To check out all
-        my playlists, feel free to check out{' '}
-        <a
-          href="https://open.spotify.com/user/max_dem"
-          className="font-medium underline decoration-green-300 decoration-2 text-gray-900 dark:text-gray-100"
-        >
-          my Spotify profile
-        </a>
-        .
+        My most played tracks on Spotify over the last 4 weeks.
       </p>
       <TopTracks />
 
